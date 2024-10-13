c203733a54938ff716a57f56d2cbc1a91abd78c8
Jonathan Borja <jborja.one@gmail.com>
2024-10-13 13:13:41 -0600

    completed exercise

---
+++ Classes/Car.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Classes;
+
+public class Car
+{
+    public string Make { get; set; }
+    public string Model { get; set; }
+    public int Year { get; set; }
+}
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var car = new Car();
+            car.Make = "Toyota";
+            car.Model = "Camry";
+            car.Year = 2021;
+
+            Console.WriteLine($"Make: {car.Make}");
+            Console.WriteLine($"Model: {car.Model}");
+            Console.WriteLine($"Year: {car.Year}");
+
         }
     }
 }
